d26c40f904c1348264d709f61d863e263dc26faf
TheMuppet <luca.falkner@tuta.io>
2025-12-05 10:53:14 +0100

    adjust dino

--- dino_v3.py
+++ dino_v3.py
@@ -1,22 +1,18 @@
+import argparse
 import numpy as np
 import os
 import torch
 from PIL import Image
 from transformers import AutoImageProcessor, AutoModel
+from config import dataset_dir, models_dir
 import logging
 
 logging.basicConfig(level=logging.INFO)
 
-# path to folder containing all datasets
-data_path = r"D:\lisa-\Universität_2\Master\2. Semester\FM\preprocessed_datasets" #\datasetname\(test/train/valid)\pictures.npz
-# path to dinov3 (uploaded to drive as well)
-model_path = r"D:\lisa-\Universität_2\Master\2. Semester\FM\dinov3"
-# if not locally, instead load: (need access via token)
-# model_path = "facebook/dinov3-vits16-pretrain-lvd1689m"
-
+model_dir = os.path.join(models_dir, 'dinov3')
 # load model 
-processor = AutoImageProcessor.from_pretrained(model_path)
-model = AutoModel.from_pretrained(model_path)
+processor = AutoImageProcessor.from_pretrained(models_dir)
+model = AutoModel.from_pretrained(model_dir)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
@@ -32,15 +28,15 @@ def load_data(path):
 # arr = numpy array containing image values
 def preprocess_data(arr):
     img = Image.fromarray(arr.astype(np.uint8), mode="L") # convert into PIL image
-    inputs = processor(images=img, return_tensors="pt")  # handles preprocessing of input: resizing, normalization, convert to tensor -> generates dictrionary
+    inputs = processor(images=img, return_tensors="pt")  # handles preprocessing of input: resizing, normalization, convert to tensor -> generates dictionary
     return inputs 
 
 
 # generates the embedding using dinov3
 # data = images arrays loaded from pictures.npz 
-def generate_embeddings(data):
+def generate_embeddings(data, test=False):
     embeddings = {}
-
+    i = 0
     for img_id in list(data.keys()):  #[:1]: # only first image in split for testing
         arr = data[img_id]
 
@@ -52,7 +48,9 @@ def generate_embeddings(data):
         emb = outputs.pooler_output
 
         embeddings[img_id] = emb.cpu().numpy()  # als np.array speichern
-        
+        i += 1
+        if test and i >= 2:
+            break
         #print(f"Shape Embedding for {img_id}: {embeddings[img_id].shape}") #(1, 384)
         #print(f"Embedding for {img_id}: {embeddings[img_id]}")
     
@@ -60,17 +58,17 @@ def generate_embeddings(data):
 
 
 if __name__ == "__main__":
-    for folder_name in os.listdir(data_path):
-        #print("Dataset being processed:", folder_name)
-        logging.info(f"Dataset being processed: {folder_name}")
-        dataset_path = os.path.join(data_path, folder_name)
+    parser = argparse.ArgumentParser(description="Calculate embeddings for dataset splits.")
+    parser.add_argument('--datasetdir', type=str, required=True, help='Path to the dataset directory')
+    parser.add_argument('--test', action='store_true', help='If set, only test code with 2 datasets')
+    args = parser.parse_args()
         
-        for split in os.listdir(dataset_path):
-            logging.info(f"Current data split being processed: {split}")
-            #print("Current data split being processed:", split)
-            img_path = os.path.join(dataset_path, split, "pictures.npz")
+    for split in os.listdir(args.datasetdir):
+        logging.info(f"Current data split being processed: {split}")
+        # print("Current data split being processed:", split)
+        img_path = os.path.join(args.datasetdir, split, "pictures.npz")
 
-            data = load_data(img_path)
-            embeddings = generate_embeddings(data)
-            #print(embeddings)
-            np.savez_compressed(os.path.join(dataset_path, split, "embeddings.npz"), **embeddings)
+        data = load_data(img_path)
+        embeddings = generate_embeddings(data, args.test)
+        #print(embeddings)
+        np.savez_compressed(os.path.join(args.datasetdir, split, "embeddings.npz"), **embeddings)
