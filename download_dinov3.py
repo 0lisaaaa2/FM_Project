62689a96c32472c374ff2fa5e09d76050de0daa0
TheMuppet <luca.falkner@tuta.io>
2025-12-13 15:47:46 +0100

    add batch processing and bigger model

--- download_dinov3.py
+++ download_dinov3.py
@@ -2,17 +2,20 @@ import os
 from huggingface_hub import snapshot_download
 from config import models_dir
 
-def download_model(local_dir):
+def download_model(local_dir, repo_id):
     
     os.makedirs(local_dir, exist_ok=True)
 
     snapshot_download(
-        repo_id="facebook/dinov3-vits16-pretrain-lvd1689m",
+        repo_id=repo_id,
         local_dir=local_dir
     )
 
 # change model_dir
 if __name__ == "__main__":
-    model_dir = os.path.join(models_dir, 'dinov3')
-    download_model(model_dir)
+    # repo = "facebook/dinov3-vit7b16-pretrain-lvd1689m"
+    # model_dir = os.path.join(models_dir, 'dinov3')
+    repo = 'facebook/dinov3-vit7b16-pretrain-lvd1689m'
+    model_dir = os.path.join(models_dir, 'dinov3_7b')
+    download_model(model_dir, repo)
     print("Download completed")
